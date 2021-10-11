3ef68f9e2d8ec6893f42d0e847c8628dc5ff7f60
Bencio09 <lorebencini13@outlook.com>
2021-10-11 13:26:25 +0200

    First Commit

---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+    </head>
+    <body>
+        <?php
+            echo "
+                <form action='$_SERVER['SELF_PHP']'>
+                    
+                </form>
+            ";
+        ?>
+    </body>
+</htm>
