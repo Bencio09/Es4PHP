b12da45a4821f7185358a62e27c0ae318534d397
Benncio09 <lorebencini13@outlook.com>
2021-10-20 10:46:43 +0200

    commit2

--- Pages/MostraStudenti.php
+++ Pages/MostraStudenti.php
@@ -1,11 +1,18 @@
 <?php
+    session_start();
     error_reporting(E_ALL & ~E_WARNING);
     $nominativo = "";
     $genere = "";
     $genere2 = "";
 
     echo var_dump($_SESSION);
-   
+
+    if (isset($_GET["chiudi"]) && $_GET["chiudi"]==true ) {
+        
+
+    }
+
+     
     if (isset($_GET["name2"])) {
         $nominativo = $_GET["name"]. " " .$_GET["name2"]. " " .$_GET["surname"];
     }elseif (!isset($_GET["name2"])) {
@@ -28,49 +35,46 @@
     $materie = "";
     if(isset($_GET["ita"])){
         $count++;
-        $materie= $materie.$_GET["ita"];
+        $materie= $materie.$_GET["ita"]. "  ";
       
     }
     if(isset($_GET["mate"])){
         $count++;
-        $materie= $materie.$_GET["mate"];
+        $materie= $materie.$_GET["mate"]. "  ";
      
     }
     if(isset($_GET["tel"])){
         $count++;
-        $materie= $materie.$_GET["tel"];
+        $materie= $materie.$_GET["tel"]. "  ";
        
     }
     if(isset($_GET["inf"])){
         $count++;
-        $materie= $materie.$_GET["inf"];
+        $materie= $materie.$_GET["inf"]. "  ";
        
     }
     if(isset($_GET["tpsit"])){
         $count++;
-        $materie= $materie.$_GET["tpsit"];
+        $materie= $materie.$_GET["tpsit"]. "  ";
        
     }
     if(isset($_GET["sist&reti"])){
         $count++;
-        $materie= $materie.$_GET["sist&reti"];
+        $materie= $materie.$_GET["sist&reti"]. "  ";
         
     }
     if(isset($_GET["ing"])){
         $count++;
-        $materie= $materie.$_GET["ing"];
+        $materie= $materie.$_GET["ing"]. "  ";
         
     }
 
     if($count >= 3){
-        $dati = "L" . $genere . " student" . $genere2 . $nominativo . " non è stat" . $genere . " ammess" . $genere;
-        echo "<br>";
+        $dati = "L" . $genere . " student" . $genere2 . $nominativo . " non è stat" . $genere . " ammess" . $genere . ".<br>";
     }elseif($count <= 2){
-        $dati = "L" . $genere . " student" . $genere2 . $nominativo . " è stat" . $genere . " ammess" . $genere . " con debito in " . $materie;
-        echo "<br>";
+        $dati = "L" . $genere . " student" . $genere2 . $nominativo . " è stat" . $genere . " ammess" . $genere . " con debito in " . $materie . ".<br>";
     }elseif($count == 0){
-        $dati = "L" . $genere . " student" . $genere2 . $nominativo . " è stat" . $genere . " ammess" . $genere;
-        echo "<br>";
+        $dati = "L" . $genere . " student" . $genere2 . $nominativo . " è stat" . $genere . " ammess" . $genere . ".<br>";
     }
 
     $_SESSION["DATI"] = $_SESSION["DATI"] . $dati;
@@ -79,8 +83,12 @@
     echo "
         <div class='container' id='ins' style='margin-top: 10%;'>
             $_SESSION[DATI]
-            <button type='button' class='btn btn-success' href='../Index.php'>Esito Finale</button>
-        </div>
+            <form method='GET'>
+                <button type='button' class='btn btn-success' href='$_SERVER[PHP_SELF]?' name='fine' value='true' chiudiSessione 'true'>Esito Finale</button>
+                <button type='button' class='btn btn-success' href='../Index.php'>Aggiungi Studente</button>;
 
+
+        </div>
     ";
+    if()
 ?>
